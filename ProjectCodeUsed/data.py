b285a0503cb9af0453b4ac6a87ed559e1c42b195
kdivringi <kdivringi@gmail.com>
2015-04-30 23:26:12 -0700

    Committed by Udacity

---
+++ ProjectCodeUsed/data.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import xml.etree.ElementTree as ET
+import pprint
+import re
+import codecs
+import json
+"""
+Your task is to wrangle the data and transform the shape of the data
+into the model we mentioned earlier. The output should be a list of dictionaries
+that look like this:
+
+{
+"id": "2406124091",
+"type: "node",
+"visible":"true",
+"created": {
+          "version":"2",
+          "changeset":"17206049",
+          "timestamp":"2013-08-03T16:43:42Z",
+          "user":"linuxUser16",
+          "uid":"1219059"
+        },
+"pos": [41.9757030, -87.6921867],
+"address": {
+          "housenumber": "5157",
+          "postcode": "60625",
+          "street": "North Lincoln Ave"
+        },
+"amenity": "restaurant",
+"cuisine": "mexican",
+"name": "La Cabana De Don Luis",
+"phone": "1 (773)-271-5176"
+}
+
+You have to complete the function 'shape_element'.
+We have provided a function that will parse the map file, and call the function with the element
+as an argument. You should return a dictionary, containing the shaped data for that element.
+We have also provided a way to save the data in a file, so that you could use
+mongoimport later on to import the shaped data into MongoDB. 
+
+Note that in this exercise we do not use the 'update street name' procedures
+you worked on in the previous exercise. If you are using this code in your final
+project, you are strongly encouraged to use the code from previous exercise to 
+update the street names before you save them to JSON. 
+
+In particular the following things should be done:
+- you should process only 2 types of top level tags: "node" and "way"
+- all attributes of "node" and "way" should be turned into regular key/value pairs, except:
+    - attributes in the CREATED array should be added under a key "created"
+    - attributes for latitude and longitude should be added to a "pos" array,
+      for use in geospacial indexing. Make sure the values inside "pos" array are floats
+      and not strings. 
+- if second level tag "k" value contains problematic characters, it should be ignored
+- if second level tag "k" value starts with "addr:", it should be added to a dictionary "address"
+- if second level tag "k" value does not start with "addr:", but contains ":", you can process it
+  same as any other tag.
+- if there is a second ":" that separates the type/direction of a street,
+  the tag should be ignored, for example:
+
+<tag k="addr:housenumber" v="5158"/>
+<tag k="addr:street" v="North Lincoln Avenue"/>
+<tag k="addr:street:name" v="Lincoln"/>
+<tag k="addr:street:prefix" v="North"/>
+<tag k="addr:street:type" v="Avenue"/>
+<tag k="amenity" v="pharmacy"/>
+
+  should be turned into:
+
+{...
+"address": {
+    "housenumber": 5158,
+    "street": "North Lincoln Avenue"
+}
+"amenity": "pharmacy",
+...
+}
+
+- for "way" specifically:
+
+  <nd ref="305896090"/>
+  <nd ref="1719825889"/>
+
+should be turned into
+"node_refs": ["305896090", "1719825889"]
+"""
+from pprint import pprint
+
+lower = re.compile(r'^([a-z]|_)*$')
+lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
+
+
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+mapping = { "St": "Street",
+            "St.": "Street",
+            "Rd" : "Road",
+            "Rd.": "Road",
+            "Ave": "Avenue",
+            "Blvd":"Boulevard",
+            "Blvd.":"Boulevard",
+            "Arthur":"Arthur St",
+            "Wellesley":"Wellesley Avenue",
+            "Main":"Main Avenue",
+            }
+
+def update_name(name, mapping):
+    """If the street type is in our list of mappings, replace it. Otherwise
+    the same thing is returned"""
+    parts = name.split()
+    last = parts[-1]
+    if last in mapping:
+        parts[-1] = mapping[last]
+        name = " ".join(parts)
+    return name
+
+def process_top_special(element, node):
+  """For any top element attribute that needs to be modified in the data
+  structure, it is done here. Includes the lat/long values and everything
+  in the CREATED list of keys"""
+  if 'lat' in element.attrib and 'lon' in element.attrib:
+    pos = [float(element.attrib['lat']), float(element.attrib['lon'])]
+    node['pos'] = pos
+    element.attrib.pop('lat')
+    element.attrib.pop('lon')
+  created_d = {}
+  for key in CREATED:
+    if key in element.attrib:
+      created_d[key] = element.attrib[key]
+  if created_d: node['created'] = created_d
+  for key in CREATED: element.attrib.pop(key, None)
+  return node
+
+def process_children(element, node):
+  """Takes in a list of child tags, the json node format and returns the node
+     data structure with the nd and tag xml tags properly encoded"""
+  for child in list(element):
+    if child.tag=="nd":
+      node_refs = node.get("node_refs", []) + [child.attrib['ref']]
+      node['node_refs'] = node_refs
+    elif child.tag=="tag" and child.attrib['k'].startswith('addr:'):
+      #Special processing for address tag types
+      address = process_address(child, node.get('address', {}))
+      if address: node['address'] = address
+    elif child.tag=="tag":
+      key = child.attrib['k']
+      node[key] = child.attrib['v']
+  return node
+
+def process_address(child, address):
+  """Special handling for address fields as per the data structure description
+  in the top of this module. Also corrects the street name abbreviation
+  problems that we indicate."""
+  key = child.attrib['k'][5:]
+  val = child.attrib['v']
+  if problemchars.search(key):
+    return None
+  elif lower_colon.match(key):
+    return None
+  elif key=="street":
+    #Special handling for bad street names
+    address[key] = update_name(val, mapping)
+  else:
+    address[key] = val
+  return address
+
+def shape_element(element):
+  """Called on each element found in the file"""
+    node = {}
+    if element.tag == "node" or element.tag == "way" :
+        node['type'] = element.tag
+        # First process all of the special attributes
+        node = process_top_special(element, node)
+        # Then the rest of the attributes
+        for key in element.attrib:
+          node[key] = element.attrib[key]
+        # If it's not empty, process child elements
+        if element.text!=None:
+            node = process_children(element, node)
+        return node
+    else:
+        return None
+
+
+def process_map(file_in, pretty = False):
+    # You do not need to change this file
+    file_out = "{0}.json".format(file_in)
+    data = []
+    with codecs.open(file_out, "w") as fo:
+        for _, element in ET.iterparse(file_in):
+            el = shape_element(element)
+            if el:
+                data.append(el)
+                if pretty:
+                    fo.write(json.dumps(el, indent=2)+"\n")
+                else:
+                    fo.write(json.dumps(el) + "\n")
+    return data
+
+def test():
+    # NOTE: if you are running this code on your computer, with a larger dataset, 
+    # call the process_map procedure with pretty=False. The pretty=True option adds 
+    # additional spaces to the output, making it significantly larger.
+    data = process_map('OSM_data.xml', True)
+    #pprint.pprint(data)
+    
+
+if __name__ == "__main__":
+    test()
